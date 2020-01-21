a760c111c475a9c33e6cb88349c55f9f1ed76419
alanmxll <alanmaxwellantunes@gmail.com>
2020-01-21 17:42:56 -0300

    middlewares defined

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 // routes
 app.use(require('./routes/index'));
 
