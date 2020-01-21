f32cf8e2552c38a5bd9c068c9743d5f13b965c55
alanmxll <alanmaxwellantunes@gmail.com>
2020-01-21 18:09:08 -0300

    port:3000 changed to port:4000

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ app.use(express.urlencoded({extended: false}));
 // routes
 app.use(require('./routes/index'));
 
-app.listen(3000);
-console.log('Server on port 3000');
+app.listen(4000);
+console.log('Server on port 4000');
