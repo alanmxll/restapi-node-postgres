b312db4cea0711329d192d710f48579c3aa5affb
alanmxll <alanmaxwellantunes@gmail.com>
2020-01-21 23:03:15 -0300

    function updateBar created.

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -40,6 +40,27 @@ const createBar = async (req, res) => {
     });
 };
 
+const updateBar = async (req, res) => {
+    const id = req.params.id_bar;
+    const { weight_bar,
+            weighing_date,
+            epoxi,
+            pu,
+            repainting } = req.body;
+    const response = await pool.query(
+        'UPDATE bars SET weight_bar = $1, weighing_date = $2, epoxi = $3, pu = $4, repainting = $5 WHERE id_bar = $6', [
+            weight_bar,
+            weighing_date,
+            epoxi,
+            pu,
+            repainting,
+            id
+        ]
+    );
+    console.log(response);
+    res.json('Bar Updated Successfully.');
+}
+
 const deleteBar = async (req, res) => {
     const id = req.params.id_bar;
     const response = await pool.query('DELETE FROM bars WHERE id_bar = $1', [id]);
@@ -51,5 +72,6 @@ module.exports = {
     getBars,
     getBarById,
     createBar,
-    deleteBar
+    deleteBar,
+    updateBar
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getBars, createBar, getBarById, deleteBar } = require('../controllers/index.controller')
+const { getBars, createBar, getBarById, deleteBar, updateBar } = require('../controllers/index.controller')
 
 router.get('/bars', getBars);
 router.get('/bars/:id_bar', getBarById);
 router.post('/bars', createBar);
+router.put('/bars/:id_bar', updateBar);
 router.delete('/bars/:id_bar', deleteBar);
 
 module.exports = router;
