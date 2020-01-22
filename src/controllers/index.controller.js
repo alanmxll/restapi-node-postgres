03fa8d0254b74a5584c6ce44e08a8896b1b7e8bb
alanmxll <alanmaxwellantunes@gmail.com>
2020-01-21 21:32:30 -0300

    created the post route

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -12,8 +12,29 @@ const pool = new Pool({
 const getBars = async (req, res) => {
     const response = await pool.query('SELECT * FROM bars');
     res.status(200).json(response.rows);
-}
+};
+
+const createBar = async (req, res) => {
+    const { weight_bar,
+            weighing_date,
+            epoxi,
+            pu,
+            repainting } = req.body;
+
+    const response = await pool.query(
+        'INSERT INTO bars (weight_bar, weighing_date, epoxi, pu, repainting) VALUES ($1, $2, $3, $4, $5)',
+        [weight_bar, weighing_date, epoxi, pu, repainting]
+    );
+    console.log(response);
+    res.json({
+        message: 'Bar Added Sucessfully',
+        body: {
+            bar: {weight_bar, weighing_date, epoxi, pu, repainting}
+        }
+    });
+};
 
 module.exports = {
-    getBars
+    getBars,
+    createBar
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getBars } = require('../controllers/index.controller')
+const { getBars, createBar } = require('../controllers/index.controller')
 
 router.get('/bars', getBars);
+router.post('/bars', createBar);
 
 module.exports = router;
